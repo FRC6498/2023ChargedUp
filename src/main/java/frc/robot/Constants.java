6b83a84c2d296c42466c374126cfe64e078876c1
Jack6498 <webbjt07@gmail.com>
2023-01-14 11:06:48 -0600

    change CAN ID's

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,8 +3,8 @@ package frc.robot;
 public class Constants {
     public static final class DriveConstants {
         public static final int Left_Front_ID = 1;
-        public static final int Right_Front_ID = 2;
-        public static final int Left_Back_ID = 3;
+        public static final int Right_Front_ID = 3;
+        public static final int Left_Back_ID = 2;
         public static final int Right_Back_ID = 4;
         public static final int Shifter_Forward_Channel = 0;
         public static final int Shifter_Reverse_Channel = 1;
