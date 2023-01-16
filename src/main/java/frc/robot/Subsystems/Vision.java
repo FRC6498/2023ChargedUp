9a2c66b6c2eb362cd407e28a4ae259da9ff64489
DeltaDizzy <EJBraswell@gmail.com>
2023-01-16 11:23:12 -0600

    update photon to photonposeestimator

--- src/main/java/frc/robot/Subsystems/Vision.java
+++ src/main/java/frc/robot/Subsystems/Vision.java
@@ -4,47 +4,40 @@
 
 package frc.robot.Subsystems;
 
-import java.util.List;
 import java.util.Optional;
 
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
-import org.photonvision.RobotPoseEstimator;
-import org.photonvision.RobotPoseEstimator.PoseStrategy;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
-import edu.wpi.first.math.Pair;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.wpilibj.Timer;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.VisionConstants;
 
 public class Vision extends SubsystemBase {
   PhotonCamera camera;
-  RobotPoseEstimator poseEstimator;
-  Pair<Pose2d, Double> currentFieldPose;
+  PhotonPoseEstimator poseEstimator;
+  EstimatedRobotPose currentFieldPose;
   /** Creates a new Vision. */
 
   public Vision() {
     camera = new PhotonCamera(VisionConstants.cameraName);
-    poseEstimator = new RobotPoseEstimator(VisionConstants.tagLayout, PoseStrategy.AVERAGE_BEST_TARGETS, List.of(
-      new Pair<PhotonCamera, Transform3d>(camera, VisionConstants.robotToCamera)
-      ));
+    poseEstimator = new PhotonPoseEstimator(VisionConstants.tagLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, camera, VisionConstants.robotToCamera);
   }
 
-  public Pair<Pose2d,Double> getCurrentPoseEstimate() {
+  public EstimatedRobotPose getCurrentPoseEstimate() {
     return currentFieldPose;
   }
   
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    Optional<Pair<Pose3d,Double>> poseResult = poseEstimator.update();
+    Optional<EstimatedRobotPose> poseResult = poseEstimator.update();
     if (poseResult.isPresent()) {
-      currentFieldPose = new Pair<Pose2d, Double>(poseResult.get().getFirst().toPose2d(), Timer.getFPGATimestamp() - poseResult.get().getSecond());
+      currentFieldPose = poseResult.get();
     } else {
-      currentFieldPose = new Pair<Pose2d,Double>(null, 0.0);
+      currentFieldPose = new EstimatedRobotPose(new Pose3d(), -1);
     }
   }
 }
