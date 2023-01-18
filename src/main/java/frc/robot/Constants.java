ff8b9713ec5f5fdd315c0de2b1834670a306c85d
DeltaDizzy <EJBraswell@gmail.com>
2023-01-18 13:48:13 -0600

    added docs and tag rotation to tag pose generator

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -45,24 +45,33 @@ public class Constants {
         public static final String cameraName = "visionCam";
         private static final double fieldLength = Units.inchesToMeters((54*12) + 3.25);
         private static final double fieldWidth = Units.inchesToMeters((26*12) + 3.5);
-        // TODO: measure origin to x translation
-        private static final Translation2d originToX = new Translation2d(4.5, 2);
+        
         // TODO: handle alliance switching
         public static final AprilTagFieldLayout tagLayout = new AprilTagFieldLayout(
             List.of(
-              new AprilTag(1, createTagPose(Units.inchesToMeters(440.625), Rotation2d.fromDegrees(63), Units.inchesToMeters(23.75))),//new Pose3d(0, 0, Units.inchesToMeters(23.75), new Rotation3d(VecBuilder.fill(0, 0, 1), 0))),
-              new AprilTag(2, createTagPose(Units.inchesToMeters(370), Rotation2d.fromDegrees(123), Units.inchesToMeters(51.5))),
-              new AprilTag(3, createTagPose(Units.inchesToMeters(484.625), Rotation2d.fromDegrees(91), Units.inchesToMeters(26.25))),
-              new AprilTag(5, createTagPose(Units.inchesToMeters(271), Rotation2d.fromDegrees(33), Units.inchesToMeters(44))),
-              new AprilTag(6, createTagPose(Units.inchesToMeters(273), Rotation2d.fromDegrees(322), Units.inchesToMeters(57.0625))),
-              new AprilTag(7, createTagPose(Units.inchesToMeters(211.5), Rotation2d.fromDegrees(0), Units.inchesToMeters(43)))
+              new AprilTag(1, createTagPose(Units.inchesToMeters(440.625),  Rotation2d.fromDegrees(63), 23.75,  Rotation2d.fromDegrees(0))),
+              new AprilTag(2, createTagPose(Units.inchesToMeters(370),      Rotation2d.fromDegrees(123),51.5,   Rotation2d.fromDegrees(0))),
+              new AprilTag(3, createTagPose(Units.inchesToMeters(484.625),  Rotation2d.fromDegrees(91), 26.25,  Rotation2d.fromDegrees(0))),
+              new AprilTag(5, createTagPose(Units.inchesToMeters(271),      Rotation2d.fromDegrees(33), 44,     Rotation2d.fromDegrees(0))),
+              new AprilTag(6, createTagPose(Units.inchesToMeters(273),      Rotation2d.fromDegrees(322),57.0625,Rotation2d.fromDegrees(0))),
+              new AprilTag(7, createTagPose(Units.inchesToMeters(211.5),    Rotation2d.fromDegrees(0),  43,     Rotation2d.fromDegrees(0)))
             ), VisionConstants.fieldLength, VisionConstants.fieldWidth
         );
 
         // TODO: fill out robotToCamera transform once robot is designed
         public static final Transform3d robotToCamera = new Transform3d();
 
-        private static Pose3d createTagPose(double distanceMeters, Rotation2d measuredAngle, double heightMeters) {
+        // TODO: measure origin to x translation
+        private static final Translation2d originToX = new Translation2d(0, new Rotation2d(0));
+
+        /**
+         * Given measured distance, angle, and orientation from a predefined measurement point in a WNU coordinate system, determine the 3d pose in the WPILib field coordinate system
+         * @param distanceInches Hypotenuse distance from the measurement point to the tag's 2d location in inches
+         * @param measuredAngle WNU angle to the tag, measured at the measurement point
+         * @param heightInches Height of the tag centerpoint above the ground in inches
+         * @param tagRotation Angle the tag is facing in the XY plane (i.e. a rotation around the Z axis) in the field coordinate system (0 = towards far wall, 90 = towards archery windows, 180 = towards janitor room, 270 = towards bleachers)
+         */
+        private static Pose3d createTagPose(double distanceInches, Rotation2d measuredAngle, double heightInches, Rotation2d tagRotation) {
             // Step 1.start at origin
             Pose2d originPose = new Pose2d();
             // Step 2.  translate to measurement point
@@ -71,9 +80,9 @@ public class Constants {
             // Step 3. rotate tag angle to new coord system
             Rotation2d angleToTag = Rotation2d.fromDegrees((measuredAngle.getDegrees() - 90) * -1);
             // Step 4. find and apply translation to tag (on ground)
-            Pose2d tagPose = center.transformBy(new Transform2d(new Translation2d(distanceMeters, angleToTag), new Rotation2d()));
-            // Step 5. move pose up to tag height
-            Pose3d tagPose3d = new Pose3d(tagPose.getX(), tagPose.getY(), heightMeters, new Rotation3d(VecBuilder.fill(0, 1, 0), Units.degreesToRadians(-90)));
+            Pose2d tagPose = center.transformBy(new Transform2d(new Translation2d(Units.inchesToMeters(distanceInches), angleToTag), new Rotation2d()));
+            // Step 5. move pose up to tag height and apply orientation
+            Pose3d tagPose3d = new Pose3d(tagPose.getX(), tagPose.getY(), Units.inchesToMeters(heightInches), new Rotation3d(VecBuilder.fill(0, 0, 1), tagRotation.getRadians()));
             // Done!
             return tagPose3d;
         }
